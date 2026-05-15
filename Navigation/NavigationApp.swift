5385c7db702d3eebfc7f3b5734ea719d5e5dee7b
Hana Tokumitsu <nishinokana87326@gmail.com>
2026-05-15 13:21:53 +0900

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by 徳光はな on 2026/05/15.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
